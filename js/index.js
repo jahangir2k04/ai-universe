e242f4fc159bd118a1588a91057c3c93837058e7
jahangir2k04 <jahangir2k04gmail.com>
2023-03-03 23:25:54 +0600

    features added

--- js/index.js
+++ js/index.js
@@ -6,20 +6,21 @@ const loadCardData = async () => {
 }
 const displayCardData = cards => {
     const universeContainer = document.getElementById('ai-universe-hub-container');
-
-    // const featureItem = cards.
-
+    
     cards.forEach(card => {
         console.log(card.features);
+        const featureItem = card.features.map(item =>`<li>${item}</li>`).join('');
+
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('col');
         cardDiv.innerHTML = `
             <div class="card h-100 p-3">
-                <img src="${card.image}" class="card-img-top" alt="...">
+                <img style="height: 200px;" src="${card.image}" class="card-img-top" alt="...">
                 <div class="card-body p-0 mt-3">
                     <h5 class="card-title fw-bold">Features</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <ol class="ps-3">${featureItem}</ol>
                 </div>
+                
             </div>
         `;
         universeContainer.appendChild(cardDiv);
